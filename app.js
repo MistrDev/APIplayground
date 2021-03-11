2415778f8b20c6933bb0f02a3de363c3d45e63c4
MistrDev <cwcorster123@gmail.com>
2021-03-11 09:40:39 -0600

    messing around with API's

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ axios.get('https://api.cryptonator.com/api/full/btc-usd')
 .catch(err => {
     console.log("OH NO! ERROR!", err)
 })
+
+
+// const bitcoinPrice = async () => {
+//     try {
+//      const res = axios.get('https://api.cryptonator.com/api/full/btc-usd')
+//      console.log(res.data.ticker.price)
+// } catch (e) {
+//     console.log('Oh no error!', e)
+// }
+ 
+// }

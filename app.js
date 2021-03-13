300d85e193cc9dd9d54c3d2f8ac025dc3132ad2f
MistrDev <cwcorster123@gmail.com>
2021-03-13 06:09:30 -0600

    I commented out some code because I dont know how Im want to go about fetching the price of bitcoin and saving it in a variable with axios

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-axios.get('https://api.cryptonator.com/api/full/btc-usd')
-.then( res => {
-   console.log(res.data.ticker.price)
-})
-.catch(err => {
-    console.log("OH NO! ERROR!", err)
-})
-
-
-// const bitcoinPrice = async () => {
-//     try {
-//      const res = axios.get('https://api.cryptonator.com/api/full/btc-usd')
-//      console.log(res.data.ticker.price)
-// } catch (e) {
-//     console.log('Oh no error!', e)
+// const bitBtn = document.querySelector('.bitBtn')
+// bitBtn.document{
+    
 // }
+
+// axios.get('https://api.cryptonator.com/api/full/btc-usd')
+// .then( res => {
+//    console.log(res.data.ticker.price)
+// })
+// .catch(err => {
+//     console.log("OH NO! ERROR!", err)
+// })
+
+
+const bitcoinPrice = async () => {
+    try {
+     const res = await axios.get('https://api.cryptonator.com/api/full/btc-usd')
+     console.log(res.data.ticker.price)
+} catch (e) {
+    console.log('Oh no error!', e)
+}
  
-// }
+}
+
+
+
